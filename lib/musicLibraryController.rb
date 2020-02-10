8f57795978c029bdcc68b7c87350869c0542ae18
Kyle McKenna <mckennakyler@gmail.com>
2020-02-10 07:27:33 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -18,6 +18,8 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
+    puts "What would you liketo do?"
+    
     
   end   
   
