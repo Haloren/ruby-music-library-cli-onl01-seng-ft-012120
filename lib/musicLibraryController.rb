66d10e633a41497079ac3b3c4f32c43fdd37cd12
Kyle McKenna <mckennakyler@gmail.com>
2020-02-10 07:25:44 +0000

    Automatically backed up by Learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -9,11 +9,12 @@ class MusicLibraryController
     MusicImporter.new(path).import #invokes import method  
   end 
   
-  def call #note lots of puts strings use learn to get them all at once
+  def call #note: 9 puts strings use learn (not learn --f-f) to get them all at once
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
+    puts 
     
   end   
   
